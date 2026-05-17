385d1094d950209927832e895c90bd372cff455e
Malika Shpayeva <malika.0805@icloud.com>
2026-05-17 16:42:28 +0500

    Delete src directory

--- src/src/preprocessing.py
+++
@@ -1,8 +0,0 @@
-# Data preprocessing script for MovieLens 20M
-
-# This file will be completed in Week 2.
-# Main tasks:
-# 1. Load ratings.csv
-# 2. Encode userId and movieId
-# 3. Split data into train, validation, and test sets
-# 4. Save processed data
